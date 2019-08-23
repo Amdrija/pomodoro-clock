0a626824559dd1defb271a1ee086f7bfe850b451
Amdrija <andrija.jelenkovic@protonmail.com>
2019-08-23 23:03:38 +0200

    Add layout

---
+++ main.js
@@ -0,0 +1,12 @@
+let time_div = document.querySelector("#time");
+let session_length_div = document.querySelector("#session-length");
+let break_length_div = document.querySelector("#break-length");
+let seession_plus_button = document.querySelector("#session-plus");
+let seession_minus_button = document.querySelector("#session-minus");
+let break_plus_button = document.querySelector("#break-plus");
+let break_minus_button = document.querySelector("#break-minus");
+
+let start_button = document.querySelector("#start");
+let reset_button = document.querySelector("#reset");
+let pause_button = document.querySelector("#pause");
+let stop_button = document.querySelector("#stop");
